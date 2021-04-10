2a3ae2ba8ac9cc8215af00f180826f164eaf8377
Joao Lopes <joaorfclopes@gmail.com>
2021-04-10 18:23:14 +0100

    Project added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started with Todo List App
+
+This project is a Technical Challenge as part of Edirect recruitment program.
+
+## Available Scripts
+
+In the project directory, just run:
+
+### `yarn quick-start`
+
+This will install all the dependencies and run the app.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+Hope you like it! :smile:
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [result, setResult] = useState("");
+
+  const fetchData = async () => {
+    const response = await axios.get("/api/");
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{result}</h1>
+    </div>
+  );
+}
+
+export default App;
